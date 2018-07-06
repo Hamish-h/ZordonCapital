e9c934dff0b203f18bd3a9af11fd588ed1432c86
Juan <juanmataruiz84@gmail.com>
2018-07-06 11:44:16 +0100

    db drop

--- app/server/index.js
+++ app/server/index.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) {
+    console.error(err);
+  }
+
+  const db = client.db('zordoncapital');
+  const portfolioCollection = db.collection('portfolio');
+  const portfolioRouter = createRouter(portfolioCollection)
+  app.use('/api/portfolio', portfolioRouter);
+});
 
 app.use(express.static('app/client/public'))
 
