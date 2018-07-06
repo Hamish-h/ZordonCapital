be28665e9868ebce6e751c76d7fdfc8d6036a65f
Juan <juanmataruiz84@gmail.com>
2018-07-06 16:17:03 +0100

    created form add new share

--- app/client/src/app.js
+++ app/client/src/app.js
@@ -1,9 +1,14 @@
 const Portfolio_view = require('./views/portfolio')
+const PortfolioFormView = require('./views/portfolio_form_view')
 const PortfolioModel = require('./models/portfolio')
 
 const portfolioElement = document.querySelector('#portfolio')
 const portfolioView = new Portfolio_view(portfolioElement)
 portfolioView.bindEvents()
+
+const shares = document.querySelector('#new-share');
+const portfolioFormView = new PortfolioFormView(shares);
+portfolioFormView.bindEvents();
 
 const portfolioModel = new PortfolioModel
 portfolioModel.bindEvents()
--- app/client/src/models/portfolio.js
+++ app/client/src/models/portfolio.js
@@ -8,13 +8,23 @@ const PortfolioModel = function() {
 PortfolioModel.prototype.bindEvents = function() {
   pubSub.subscribe('Portfolio:get-portfolio', () => {
     this.getPortfolio()
-  })
-}
+  });
+
+  pubSub.subscribe('PortfolioFormView:share-submitted', (evt) => {
+    this.postShare(evt.detail);
+  });
+};
 
 PortfolioModel.prototype.getPortfolio = function () {
   request.get(this.url, (error, data) => {
     pubSub.publish('Portfolio-view:portfolio-data', data)
-  })
-}
+  });
+};
+
+PortfolioModel.prototype.postShare = function (share) {
+  request.post(this.url, share, (error, data) => {
+    pubSub.publish('Portfolio-view:portfolio-data', data)
+  });
+};
 
 module.exports = PortfolioModel
