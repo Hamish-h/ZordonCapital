68d0f3570dde0844963f04b2c14a90d05d9c65ca
Colin Tindle <ampersand@underscore.space>
2018-07-09 16:19:57 +0100

    added company search form

--- app/client/src/app.js
+++ app/client/src/app.js
@@ -1,6 +1,7 @@
 const Portfolio_view = require('./views/portfolio_view')
 const PortfolioFormView = require('./views/portfolio_form_view')
 const PortfolioModel = require('./models/portfolio')
+const CompanySearchView = require('./views/company_search')
 
 const portfolioElement = document.querySelector('#portfolio')
 const portfolioView = new Portfolio_view(portfolioElement)
@@ -9,6 +10,10 @@ portfolioView.bindEvents()
 const shares = document.querySelector('#new-share');
 const portfolioFormView = new PortfolioFormView(shares);
 portfolioFormView.bindEvents();
+
+const searchElement = document.querySelector('#company-search')
+const companySearch = new CompanySearchView(searchElement)
+companySearch.bindEvents()
 
 const portfolioModel = new PortfolioModel
 portfolioModel.bindEvents()
---
+++ app/client/src/views/company_search.js
@@ -0,0 +1,15 @@
+const pubSub = require('../helpers/pubsub')
+
+const CompanySearchView = function(container){
+  this.container = container
+}
+
+CompanySearchView.prototype.bindEvents = function() {
+  this.container.addEventListener('submit', event => {
+    event.preventDefault()
+    const searchText = event.target.company_search_results.value
+    pubSub.publish('CompanySearchView:search-text', searchText)
+  })
+}
+
+module.exports = CompanySearchView
