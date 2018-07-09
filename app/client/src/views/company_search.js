1aba847257c7516d06dde7a77d7011b3f79b2659
Colin Tindle <ampersand@underscore.space>
2018-07-09 17:39:07 +0100

    company search result being rendered

--- app/client/src/views/company_search.js
+++ app/client/src/views/company_search.js
@@ -10,6 +10,29 @@ CompanySearchView.prototype.bindEvents = function() {
     const searchText = event.target.company_search_text.value
     pubSub.publish('CompanySearchView:search-text', searchText)
   })
+
+  pubSub.subscribe('Companies:search-results', event => {
+    const searchResults = event.detail
+    this.render(searchResults)
+  })
+}
+
+CompanySearchView.prototype.render = function(searchResults) {
+  const resultElement = this.container.querySelector('#company-search-results tbody')
+
+  for (searchResult of searchResults) {
+    const tr = document.createElement('tr')
+
+    const symbol = document.createElement('td')
+    symbol.textContent = searchResult.symbol
+
+    const name = document.createElement('td')
+    name.textContent = searchResult.name
+
+    tr.appendChild(symbol)
+    tr.appendChild(name)
+    resultElement.appendChild(tr)
+  }
 }
 
 module.exports = CompanySearchView
